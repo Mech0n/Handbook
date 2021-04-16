9f8594befa64992016b2336320ee9f44ddc95efe
Mech0n <qq1140181443@yeah.net>
2021-04-16 10:40:28 +0800

    Add Midnight Sun CTF 2021 brohammer WP

--- WriteUp/brohammer/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-qemu-system-x86_64 \
-    -m 512M \
-    -kernel ./kernel \
-    -initrd ./root.cpio \
-    -nographic \
-    -s \
-    -monitor /dev/null \
-    -append "nokaslr root=/dev/ram rw console=ttyS0 oops=panic paneic=1 quiet" 2>/dev/null
